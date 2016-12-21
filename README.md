07667d261015a4b29be227361d1ef959dc0b7d9a
tony2times2times <tony.peraza86@gmail.com>
2016-12-21 15:54:31 -0600

    basic styling added

--- README.md
+++ README.md
@@ -8,14 +8,14 @@
 
 [x] Each task should have an option to 'Complete' or 'Delete'.
 
-[] When a task is complete, its visual representation should change on the front end (for example, the background of the task container could change from gray to green, as well as the complete option 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete)
+[x] When a task is complete, its visual representation should change on the front end (for example, the background of the task container could change from gray to green, as well as the complete option 'checked off'. Each of these are accomplished in CSS, but will need to hook into logic to know whether or not the task is complete)
 
 [x] Whether or not a task is complete should also be stored in the database.
 
-[] Deleting a task should remove it both from the Front End as well as the Database.
+[x] Deleting a task should remove it both from the Front End as well as the Database.
 
 ###HARD MODE
-[] In whatever fashion you would like, create an 'are you sure: yes / no' option when deleting a task. Once again, you can interrupt this however you would like.
+[x] In whatever fashion you would like, create an 'are you sure: yes / no' option when deleting a task. Once again, you can interrupt this however you would like.
 
 ###PRO MODE
 [] Adjust the logic so that completed tasks are brought to the bottom of the page, where the remaining tasks left to complete are brought to the top of the list.
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,42 +1,44 @@
-$(function(){
-  console.log('Document ready!');
-  getTasks();
-  enable();
+$(function() {
+    console.log('Document ready!');
+    getTasks();
+    enable();
 });
 
-function enable(){
-$('#post_task').unbind().on('click',postTask);
-$('.complete').unbind().on('click', putTask);
-$('.delete').unbind().on('click', delTask);
-$("#new_task").unbind().keydown(function(event){
-    if(event.keyCode == 13){
-        $("#post_task").click();
-    }
-});
+function enable() {
+    $('#post_task').unbind().on('click', postTask);
+    $('.complete').unbind().on('click', putTask);
+    $('.delete').unbind().on('click', delTask);
+    $("#new_task").unbind().keydown(function(event) {
+        if (event.keyCode == 13) {
+            $("#post_task").click();
+        }
+    });
 }
 
 function getTasks() {
-  tasks=[];
+    tasks = [];
     $.ajax({
         type: "GET",
         url: '/getTasks',
         success: function(response) {
             console.log('back from getTasks: ', response);
             for (var i = 0; i < response.length; i++) {
-              tasks.push(response[i]);
-              printTasks(tasks);
-              enable();
+                tasks.push(response[i]);
+                printTasks(tasks);
+                enable();
             }
         },
         error: function() {
-                console.log("error with ajax call");
-            }
+            console.log("error with ajax call");
+        }
     });
 }
 
-function postTask(){
-  var newTask = {task:$('#new_task').val()};
-  console.log('sending a new task ' + newTask);
+function postTask() {
+    var newTask = {
+        task: $('#new_task').val()
+    };
+    console.log('sending a new task ' + newTask);
     $('#new_task').val('');
     $.ajax({
         type: "POST",
@@ -47,51 +49,63 @@ function postTask(){
             getTasks();
         },
         error: function() {
-                console.log("error with ajax call");
-            }
+            console.log("error with ajax call");
+        }
     });
 }
 
-function putTask(){
-  var updateTask = {id:this.getAttribute('data')};
-  $.ajax({
-      type: "PUT",
-      url: '/putTask',
-      data: updateTask,
-      success: function(response) {
-          console.log('back from postTasks: ', response);
-          getTasks();
-      },
-      error: function() {
-              console.log("error with ajax call");
-          }
-  });
+function putTask() {
+    var updateTask = {
+        id: this.getAttribute('data')
+    };
+    $.ajax({
+        type: "PUT",
+        url: '/putTask',
+        data: updateTask,
+        success: function(response) {
+            console.log('back from postTasks: ', response);
+            getTasks();
+        },
+        error: function() {
+            console.log("error with ajax call");
+        }
+    });
 }
 
-function delTask(){
-  var delTask = {id:this.getAttribute('data')};
-  $.ajax({
-      type: "DELETE",
-      url: '/delTask',
-      data: delTask,
-      success: function(response) {
-          console.log('back from delTasks: ', response);
-          getTasks();
-      },
-      error: function() {
-              console.log("error with ajax call");
-          }
-  });
-}
+function delTask() {
+    if (confirm("Cyberman: DELETE DELETE DELETE! Are you sure you want to DELETE this task?")) {
 
-function printTasks(tasks){
-  console.log("now printing tasks: " + tasks);
-var allTasks = '<tr><td>Task</td><td>Date Created</td><td>Complete Task</td><td>Delete Task</td>';
-  for (var i = 0; i < tasks.length; i++) {
-    allTasks += '<tr><td>' + tasks[i].task + '</td>';
-    allTasks += '<td>' + tasks[i].created + '</td>';
-    allTasks += '<td><button type="button" class="complete" data="' + tasks[i].id + '"> Complete </button> </td>';
-    allTasks += '<td><button type="button" class="delete" data="' + tasks[i].id + '"> Delete </button> </td>';
+    var delTask = {
+        id: this.getAttribute('data')
+    };
+    $.ajax({
+        type: "DELETE",
+        url: '/delTask',
+        data: delTask,
+        success: function(response) {
+            console.log('back from delTasks: ', response);
+            getTasks();
+        },
+        error: function() {
+            console.log("error with ajax call");
+        }
+    });
   }
-$('#tasks').html(allTasks);
+  else {
+    return;
+  }
+}
+
+function printTasks(tasks) {
+    console.log("now printing tasks: " + tasks);
+    var allTasks = '<table> <tr><td>Task</td><td>Complete Task</td><td>Delete Task</td>';
+    for (var i = 0; i < tasks.length; i++) {
+        allTasks += '<tr><td  id="first" class=' + tasks[i].active + '>' + tasks[i].task + '</td>';
+        allTasks += '<td><button type="button" class="complete ' + tasks[i].active + '_button" data="' +
+            tasks[i].id + '"></button> </td>';
+        allTasks += '<td><button type="button" class="delete" data="' + tasks[i].id +
+            '"></button> </td>';
+    }
+    allTasks += '</table>';
+    $('#tasks').html(allTasks);
 }
