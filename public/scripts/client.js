e589e09ae6565337fa1ddeb7467a413972488663
tony2times2times <tony.peraza86@gmail.com>
2016-12-22 15:51:26 -0600

    using jquery modal to confirm user input

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -73,27 +73,46 @@ function putTask() {
 }
 
 function delTask() {
-    if (confirm("Cyberman: DELETE DELETE DELETE! Are you sure you want to DELETE this task?")) {
-
-    var delTask = {
-        id: this.getAttribute('data')
-    };
-    $.ajax({
-        type: "DELETE",
-        url: '/delTask',
-        data: delTask,
-        success: function(response) {
-            console.log('back from delTasks: ', response);
-            getTasks();
-        },
-        error: function() {
-            console.log("error with ajax call");
-        }
-    });
-  }
-  else {
-    return;
-  }
+    $('#display').hide();
+    $('<div></div>').appendTo('body')
+        .html('<div><h6>Cyberman: DELETE DELETE DELETE! Are you sure you want to DELETE this task?</h6></div>')
+        .dialog({
+            modal: true,
+            title: 'Delete message',
+            zIndex: 10000,
+            autoOpen: true,
+            width: '50%',
+            resizable: false,
+            buttons: {
+                Yes: function() {
+                    var delTask = {
+                        id: this.getAttribute('data')
+                    };
+                    $.ajax({
+                        type: "DELETE",
+                        url: '/delTask',
+                        data: delTask,
+                        success: function(response) {
+                            console.log('back from delTasks: ', response);
+                            getTasks();
+                        },
+                        error: function() {
+                            console.log("error with ajax call");
+                        }
+                    });
+                    $(this).dialog("close");
+                    $('#display').show();
+                },
+                No: function() {
+                    $(this).dialog("close");
+                    $('#display').show();
+                }
+            },
+            close: function(event, ui) {
+                $(this).remove();
+                $('#display').show();
+            }
+        });
 }
 
 function printTasks(tasks) {
